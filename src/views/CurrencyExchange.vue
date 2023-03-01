412da6ecb76760a2fe8afd1d145873b622a2d6e0
Nguyen Thanh Tung <tung.nguyen@twentyci.co.uk>
2023-03-01 11:59:38 +0700

    Init

---
+++ src/views/CurrencyExchange.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class="col-3">
+    <Menu />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Menu from "@/components/currency_exchange/Menu.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    Menu,
+  },
+};
+</script>
