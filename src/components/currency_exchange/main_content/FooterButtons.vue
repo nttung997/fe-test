a40d4419ec0994f3e40a62a7b30034c84c6ffbe4
Nguyen Thanh Tung <tung.nguyen@twentyci.co.uk>
2023-03-02 22:49:19 +0700

    add UI

--- src/components/currency_exchange/main_content/FooterButtons.vue
+++ src/components/currency_exchange/main_content/FooterButtons.vue
@@ -1,11 +1,26 @@
 <template>
-    <div></div>
+  <div class="row">
+    <div class="col-5 pl-4">
+      <b-button class="form-control back">Back</b-button>
+    </div>
+    <div class="col-7 pr-4 pl-4">
+      <b-button class="form-control continue border-0">Continue</b-button>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style lang="scss" scoped>
-
+.continue {
+  background: #2af5d6;
+  border-radius: 4px;
+  color: #111A27;
+}
+.back {
+  border: 2px solid #2af5d6;
+  background: white;
+  color: black;
+  border-radius: 4px;
+}
 </style>
--- src/components/currency_exchange/main_content/MessageBox.vue
+++ src/components/currency_exchange/main_content/MessageBox.vue
@@ -1,11 +1,26 @@
 <template>
-    <div></div>
+  <div class="form-group mt-5">
+    <label class="px-2">Title</label>
+    <b-form-input class="py-4" placeholder="Please buy new clothes and have fun!"></b-form-input>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style lang="scss" scoped>
+.form-group{
+  border: 1px solid #2E3A42;
+  border-radius: 4px;
+  position: relative;
+}
+.form-group>label{
+  position:absolute;
+  top:-1rem;
+  left:20px;
+  background-color:white;
+}
 
+.form-group>input{
+  border:none;
+}
 </style>
